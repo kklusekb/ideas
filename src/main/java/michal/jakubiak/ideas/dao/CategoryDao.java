06831360f84307323df9694f025344c1e7ad38ce
Lenovo <kklusekb@gmail.com>
2023-09-12 20:12:21 +0200

    Obsługa kategorii

---
+++ src/main/java/michal/jakubiak/ideas/dao/CategoryDao.java
@@ -0,0 +1,11 @@
+package michal.jakubiak.ideas.dao;
+
+import michal.jakubiak.ideas.model.Category;
+
+import java.util.List;
+
+public class CategoryDao {
+    public List<Category> findAll() {
+        return null;
+    }
+}
--- src/main/java/michal/jakubiak/ideas/handlers/CategoryCommandHandler.java
+++ src/main/java/michal/jakubiak/ideas/handlers/CategoryCommandHandler.java
@@ -1,11 +1,21 @@
 package michal.jakubiak.ideas.handlers;
 
+import michal.jakubiak.ideas.dao.CategoryDao;
 import michal.jakubiak.ideas.input.UserInputCommand;
+import michal.jakubiak.ideas.model.Category;
+
+import java.util.List;
 
 public class CategoryCommandHandler extends BaseCommandHandler {
 
 
     private static final String COMMAND_NAME = "category";
+
+    private CategoryDao categoryDao;
+
+    public CategoryCommandHandler() {
+         categoryDao = new CategoryDao();
+    }
 
     @Override
     protected String getCommandName() {
@@ -14,10 +24,16 @@ public class CategoryCommandHandler extends BaseCommandHandler {
 
     @Override
     public void handle(UserInputCommand command) {
+
+        if (command.getAction()==null) System.out.println("What you want to do?");
         switch (command.getAction())
         {
+            case "" :
+                System.out.println();
             case "list" :
                 System.out.println("Category list:");
+               List<Category> categories = categoryDao.findAll();
+               categories.forEach(System.out::println);
                 break;
             case "add" :
                 System.out.println("Add category");
